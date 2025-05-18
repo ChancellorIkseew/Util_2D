620aa816a6289ef8f32dc44198dfb15676086e65
ChancellorIkseew <ikse.ksini@bk.ru>
2025-05-18 21:46:33 +1000

    add dark theme

--- Util_2D/src/cpp_ui/menu_bar.cpp
+++ Util_2D/src/cpp_ui/menu_bar.cpp
@@ -17,6 +17,8 @@ MenuBar::MenuBar(QMainWindow* parent, Workspace* workspace, Palette* palette) :
 	QMenu* view = new QMenu("view", this);
 	view->addAction("style", this, &MenuBar::setStyle);
 	addMenu(view);
+
+	setFixedHeight(24);
 }
 
 void MenuBar::openFile() {
@@ -44,8 +46,36 @@ void MenuBar::saveFileAs() {
 
 void MenuBar::setStyle() {
 	mainWindow->setStyleSheet(
-		"QMainWindow { background-color: lightblue; }"         // Main window background
-		"QPushButton { color: black; background-color: #4CAF50; }" // Default button style
-		"QPushButton:hover { background-color: #367C39; }"       // Hover effect
+		"QMainWindow { background-color: #303030; }"
+		//"QPushButton { color: black; background-color: #4CAF50; }"
+		//"QPushButton:hover { background-color: #367C39; }"
+		"QMenuBar {"
+		"    background-color: #1D1D1D;"
+		"    color: white;"
+		"    border: 2px solid #161616;"
+		"}"
+		"QMenuBar::item:selected {"
+		"    background-color: #3A5986;"
+		"    border: 1px solid #4772B3;"
+		"    border-radius: 2px;"
+		"}"
+		"QMenu {"
+		"    background-color: #141414;"
+		"    color: white;"
+		"    border: 1px solid #101010;"
+		"}"
+		"QMenu::item:selected {"
+		"    background-color: #3A5986;"
+		"    border: 2px solid #3A5986;"
+		"    border-radius: 4px;"
+		"}"
+
+		/*
+		"QToolBar {"
+		"    background-color: #1D1D1D;"
+		"    color: white;"
+		"    border: 2px solid #161616;"
+		"}"*/
+
 	);
 }
