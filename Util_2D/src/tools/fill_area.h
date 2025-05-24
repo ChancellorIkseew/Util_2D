8b5552d7e24b2ec080748444b6a04d2fc551e063
ChancellorIkseew <ikse.ksini@bk.ru>
2025-05-24 12:15:23 +1000

    implement fillArea

--- Util_2D/src/tools/fill_area.h
+++ Util_2D/src/tools/fill_area.h
@@ -1,13 +1,35 @@
 #pragma once
 #include <QtWidgets/qlabel.h>
+#include <list>
+
+static inline bool unique(const QPoint& point, const std::list<QPoint>& spot) {
+    return std::find(spot.begin(), spot.end(), point) == spot.end();
+}
 
 void fillArea(QImage& image, QPoint pixel, QRgb color) {
     QRgb preveouseColor = image.pixel(pixel);
+    QPoint imgSize(image.size().width(), image.size().height());
     //
-    for (int y = 0; y < image.height(); ++y) {
-        for (int x = 0; x < image.width(); ++x) {
-            if (image.pixel(x, y) == preveouseColor)
-                image.setPixel(x, y, color);
-        }
+    std::list<QPoint> spot;
+    spot.push_back(pixel);
+    //
+    while (!spot.empty()) {
+        QPoint it = spot.front();
+        spot.pop_front();
+        image.setPixel(it, color);
+        //
+        QPoint up    = it - QPoint(0, 1);
+        QPoint down  = it + QPoint(0, 1);
+        QPoint left  = it - QPoint(1, 0);
+        QPoint right = it + QPoint(1, 0);
+        //
+        if (up.y() >= 0 && image.pixel(up) == preveouseColor && unique(up, spot))
+            spot.push_back(up);
+        if (down.y() < imgSize.y() && image.pixel(down) == preveouseColor && unique(down, spot))
+            spot.push_back(down);
+        if (left.x() >= 0 && image.pixel(left) == preveouseColor && unique(left, spot))
+            spot.push_back(left);
+        if (right.x() < imgSize.x() && image.pixel(right) == preveouseColor && unique(right, spot))
+            spot.push_back(right);
     }
 }
