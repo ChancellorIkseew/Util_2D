2d9ddc12eaed4c1a88cd9067d50739ffeab9a05d
ChancellorIkseew <ikse.ksini@bk.ru>
2025-05-26 18:30:33 +1000

    workspace bug fix

--- Util_2D/src/cpp_ui/workspace.cpp
+++ Util_2D/src/cpp_ui/workspace.cpp
@@ -3,59 +3,49 @@
 #include <QtGui/QMouseEvent>
 #include <iostream>
 
-static QPoint _mousePos;
-static QPoint _startDraggingPos;
-
-
 Workspace::Workspace(QWidget* parent) : QGraphicsView(parent), _scene(this) {
 	setStyleSheet(QString("QWidget {background-color: #1D1D1D; }"));
 	setScene(&_scene);
-	setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-	setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+	setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+	setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
 	setFrameStyle(QFrame::NoFrame);
 }
 
 void Workspace::scale(const int modifier) {
 	_scale = static_cast<qreal>(modifier) / 100.0;
 	setTransform(QTransform::fromScale(_scale, _scale), false);
+	//QTransform tr = QTransform(_scale, 0, 0, _scale, 100, 100);
+	//setTransform(tr, false);
 }
 
 void Workspace::mousePressEvent(QMouseEvent* event) {
 	QGraphicsView::mousePressEvent(event);
 	if (!_item)
 		return;
-	Qt::MouseButton button = event->button();
-	_mousePos = relativePos(event->pos());
-
-	_selectedPixel.setX(_image.size().width() / 2 + static_cast<qreal>(_mousePos.x()) / _scale);
-	_selectedPixel.setY(_image.size().height() / 2 + static_cast<qreal>(_mousePos.y()) / _scale);
+	QPoint p = event->pos();
+	QPointF pf = mapToScene(p);
+	_selectedPixel = QPoint(pf.x(), pf.y());
 	
-	//std::cout << _selectedPixel.x() << " " << _selectedPixel.y() << '\n';
-	/*
-	if (!_isDragging)
-		_startDraggingPos = _mousePos;
-
-	if (_isDragging) {
-		QPoint delta = _startDraggingPos - _mousePos;
-		std::cout << "delta " << delta.x() << " " << delta.y() << '\n';
-		
-	}
-	*/
 	if (event->button() == Qt::MouseButton::LeftButton)
 		handler();
-	if (event->button() == Qt::MouseButton::MiddleButton)
+	if (event->button() == Qt::MouseButton::MiddleButton) {
 		_isDragging = true;
-
-	std::cout << "size " << size().width() << " " << size().height() << '\n';
-	std::cout << "pos " << event->pos().x() << " " << event->pos().y() << '\n';
-	std::cout << "rel " << _mousePos.x() << " " << _mousePos.y() << '\n';
-
+		setCursor(Qt::ClosedHandCursor);
+	}
 }
 
 void Workspace::mouseReleaseEvent(QMouseEvent* event) {
 	QGraphicsView::mouseReleaseEvent(event);
-	if (event->button() == Qt::MouseButton::MiddleButton)
+	if (event->button() == Qt::MouseButton::MiddleButton) {
 		_isDragging = false;
+		setCursor(Qt::ArrowCursor);
+	}
+}
+
+void Workspace::mouseMoveEvent(QMouseEvent* event) {
+	if (!_isDragging)
+		return;
+	//scroll(event->pos().x() / 2, event->pos().y() / 2);
 }
 
 void Workspace::wheelEvent(QWheelEvent* event) {
